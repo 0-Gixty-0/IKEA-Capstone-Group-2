b8c3d0b55946dbd9927efcbd26bb37389a6458df
Adam Kvarnsund <adam.kvarnsund@outlook.com>
2024-09-26 15:41:13 +0200

    feat: add hero section

--- guida/src/app/page.tsx
+++ guida/src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div></div>
+    <div className={styles.fullScreen}>
+      <div className={styles.postContainer}>
+
+      </div>
+    </div>
   );
 }
