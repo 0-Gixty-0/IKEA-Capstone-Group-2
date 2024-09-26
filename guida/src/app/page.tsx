f86ccc895362dfc4491a7b9fbee47a0cfc12997f
ErikA <erik.andreasson73@gmail.com>
2024-09-26 12:09:19 +0200

    refactor(edit-page): Add simple get post route and dispay post in form

--- guida/src/app/page.tsx
+++ guida/src/app/page.tsx
@@ -1,21 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import EditArticle from "./edit-page";
-
-const mockArticle = {
-  title: "Sample Article",
-  content: "This is a sample article content.",
-  author: "Albin"
-};
-
-const handleSave = (updatedArticle) => {
-  console.log("Article updated:", updatedArticle);
-};
+import EditArticle from "./edit/page";
 
 export default function Home() {
   return (
     <div>
-      EditArticle article={mockArticle} onSave={handleSave} />
+
     </div>
   );
 
