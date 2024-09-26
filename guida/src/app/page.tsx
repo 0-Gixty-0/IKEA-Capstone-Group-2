bdf4e46898fd77b3992891d351e79c26932e2e88
SkepparAbbe <albske12@gmail.com>
2024-09-26 11:00:53 +0200

    new branch and shit code

--- guida/src/app/page.tsx
+++ guida/src/app/page.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import EditArticle from "./edit-page";
+
+const mockArticle = {
+  title: "Sample Article",
+  content: "This is a sample article content.",
+  author: "Albin"
+};
+
+const handleSave = (updatedArticle) => {
+  console.log("Article updated:", updatedArticle);
+};
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      EditArticle article={mockArticle} onSave={handleSave} />
+    </div>
   );
+
 }
