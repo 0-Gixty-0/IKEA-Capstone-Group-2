076e153193a05be90b08c37d5d75f758698aeb50
ErikA <erik.andreasson73@gmail.com>
2024-10-17 11:45:59 +0200

    feature(SignOutModal): Add SignOutModal.tsx

---
+++ guida/src/app/components/SignOutModal/SignOutModal.tsx
@@ -0,0 +1,27 @@
+"use client"
+import { signOut } from "next-auth/react"
+import styles from "./styles.module.css"
+import React from "react";
+
+interface Props {
+    onClose: () => void;
+}
+
+export default function SignOutModal(props : Props) {
+    const {onClose} = props
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <button className={styles.closeButton} onClick={onClose}>
+                    &times;
+                </button>
+                <h2>Sign Out</h2>
+                <hr/>
+                <h3>Are you sure you want to sign out?</h3>
+                <button className={styles.submitButton} onClick={() => signOut({redirectTo: '/login'})}>Sign Out
+                </button>
+            </div>
+        </div>
+    )
+}
