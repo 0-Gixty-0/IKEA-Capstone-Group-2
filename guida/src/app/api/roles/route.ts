3334974d11500a1d2e529d36ccd66c4b58761b40
SkepparAbbe <albske12@gmail.com>
2024-10-21 15:06:12 +0200

    (feat) API enpoint for roles extended so you can request both id or rolename

--- guida/src/app/api/roles/route.ts
+++ guida/src/app/api/roles/route.ts
@@ -3,6 +3,12 @@ import prisma from "@/db"
 import { NextResponse } from "next/server"
 
 
+/**
+ * API endpoint for getting a role by id or name.
+ * @param request 
+ * @returns 
+ */
+
 export async function GET(request : Request) {
     try {
         const session = await auth()
@@ -10,10 +16,11 @@ export async function GET(request : Request) {
         if (session) {
             const { searchParams } = new URL(request.url)
             const roleId = searchParams.get('id')
+            const roleName = searchParams.get('name')
             
 
             if (roleId) {
-                const role = await prisma.post.findUnique({
+                const role = await prisma.role.findUnique({
                     where: {id: Number(roleId)}
                 })
 
@@ -22,12 +29,27 @@ export async function GET(request : Request) {
                 }
 
                 return NextResponse.json({
-                    message: `Successfully retrieved post with id: ${roleId}`,
+                    message: `Successfully retrieved role with id: ${roleId}`,
                     role: role
                 }, {status: 200})
             }
-        } else {
-            return NextResponse.json({ error: 'You must be logged in!' }, { status: 405 })
+            if (roleName) {
+                const role = await prisma.role.findUnique({
+                    where: {name: roleName}
+                })
+
+                if (!role) {
+                    return NextResponse.json({ error: 'Role not found' }, { status: 404 });
+                }
+
+                return NextResponse.json({
+                    message: `Successfully retrieved role with name: ${roleName}`,
+                    role: role
+                }, {status: 200})
+            } 
+            else {
+                return NextResponse.json({ error: 'You must be logged in!' }, { status: 405 })
+            }
         }
     } catch (error) {
         return NextResponse.json({ error: 'Something went wrong when fetching post!' }, { status: 500 });
