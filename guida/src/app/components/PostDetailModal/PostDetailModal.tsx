8974e5c5feada6d3d80934ea979d78e99c79c32a
IsakGenberg <isakgenberg00@gmail.com>
2024-10-22 14:17:43 +0200

    testing pdf functionality
    This will be changed later, only testing having pdfs in the repo.

--- guida/src/app/components/PostDetailModal/PostDetailModal.tsx
+++ guida/src/app/components/PostDetailModal/PostDetailModal.tsx
@@ -22,6 +22,11 @@ const PostDetailModal: React.FC<ModalProps> = ({
         <ReadButton postId={postId} onRead={onRead} />{" "}
         {children}{" "}
         {/* Display the children passed, which can be PostForm or post details */}
+        <iframe
+          src="https://www.antennahouse.com/hubfs/xsl-fo-sample/pdf/basic-link-1.pdf"
+          className={styles.pdfViewer}
+          title="Collision PDF"
+        />
       </div>
     </div>
   );
