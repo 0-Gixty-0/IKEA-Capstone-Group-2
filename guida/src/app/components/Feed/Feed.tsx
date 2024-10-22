e86f64a2b39f0b3c0d074df3bb44a937d0415fff
ErikA <erik.andreasson73@gmail.com>
2024-10-22 18:44:53 +0200

    refactor(PostSearchBar) Working simple title only search

--- guida/src/app/components/Feed/Feed.tsx
+++ guida/src/app/components/Feed/Feed.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.css";
 import SkeletonList from "@/app/components/SkeletonList/SkeletonList";
 import PostList from "@/app/components/PostList/PostList";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Post} from "@/types";
 import PostSearchBar from "@/app/components/PostSearchBar/PostSearchBar";
 
@@ -27,18 +27,31 @@ interface FeedProps {
  * @constructor
  */
 export default function Feed({title, loadingPosts, error, posts, emptyMessage}: FeedProps) {
+    const [displayedPosts, setDisplayedPosts] = useState<Post[]>(posts)
+    const [showNoSearchResults, setShowNoSearchResults] = useState<boolean>(false)
+
+    useEffect(() => {
+        setDisplayedPosts(posts)
+    }, [posts]);
+
     return (
         <div className={styles.feedContainer}>
             <h2 id={styles.feedTitle}>{title}</h2>
-            <PostSearchBar></PostSearchBar>
+            <PostSearchBar
+                entries={posts}
+                setEntries={setDisplayedPosts}
+                setShowNoSearchResults={setShowNoSearchResults}>
+            </PostSearchBar>
             {loadingPosts
                 ? (<SkeletonList></SkeletonList>)
                 : (error)
                     ? (<h3>{error}</h3>)
-                    : (posts && posts.length > 0)
-                        ? (<PostList posts={posts} handlePostClick={() => {
+                    : (displayedPosts && displayedPosts.length > 0)
+                        ? (<PostList posts={displayedPosts} handlePostClick={() => {
                         }}></PostList>)
-                        : (<h3 id={styles.emptyMessage}>{emptyMessage}</h3>)}
+                        : (showNoSearchResults)
+                            ? (<h3 id={styles.emptyMessage}>No posts match your search!</h3>)
+                            : (<h3 id={styles.emptyMessage}>{emptyMessage}</h3>)}
         </div>
     )
 }
