dffa9302f50d40fedd5f5827331b09687d100ba0
ErikA <erik.andreasson73@gmail.com>
2024-10-25 17:49:04 +0200

    feature(readRequests) Add ProgressBar.tsx

--- guida/src/app/components/PostStatsItem/PostStatsItem.tsx
+++ guida/src/app/components/PostStatsItem/PostStatsItem.tsx
@@ -1,5 +1,9 @@
 import {Post} from "@/types";
 import {usePostStats} from "@/hooks/usePostStats";
+import React from "react";
+import styles from './PostStatsItem.module.css'
+import Preloader from "@/app/components/Preloader/Preloader";
+import ProgressBar from "@/app/components/ProgressBar/ProgressBar";
 
 interface IPostStatsItem {
     post: Post;
@@ -9,8 +13,19 @@ export default function PostStatsItem({post}: IPostStatsItem) {
     const {stats, loading, error} = usePostStats(post.id)
 
     return (
-        <li>
-            {!loading && <h2>{JSON.stringify(stats)}</h2>}
+        <li className={styles.statsContainer}>
+            {loading
+                ? (<Preloader></Preloader>)
+                : (stats
+                        ? (<div className={styles.statsContent}>
+                            <h2>Reading Progress</h2>
+                            <ProgressBar a={stats?.numRead} b={stats?.totalAssigned}/>
+                            </div>)
+                        : <div className={styles.statsContent}>
+                            <h1>{error}</h1>
+                        </div>
+                )
+            }
         </li>
     )
 }
